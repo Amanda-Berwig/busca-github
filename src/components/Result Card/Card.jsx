fd7563633755424ae22682922f9f793c4d5c6827
Amanda Berwig <amanda.berwig@gmail.com>
2025-04-09 11:27:05 -0300

    Refatora o componente Card para tornar a imagem do avatar clicável, redirecionando para o perfil do usuário no GitHub.

--- src/components/Result Card/Card.jsx
+++ src/components/Result Card/Card.jsx
@@ -1,11 +1,20 @@
 function Card({ dados }) {
   return (
     <div className="flex h-auto w-2xl items-center gap-8 rounded-3xl bg-[#D9D9D9] px-8 py-4">
-      <img
-        src={dados.avatar_url}
-        alt="avatar"
-        className="h-52 w-52 rounded-full border-2 border-[#005CFF]"
-      />
+      <a
+        href={`https://github.com/${dados.login}`}
+        target="_blank"
+        rel="noopener noreferrer"
+        title="Clique para acessar o perfil"
+        className="group relative"
+      >
+        <img
+          src={dados.avatar_url}
+          alt="avatar"
+          className="h-52 w-52 rounded-full border-2 border-[#005CFF] transition-transform duration-300 hover:scale-110"
+        />
+      </a>
+
       <div>
         <h2 className="text-xl font-bold text-[#005CFF]">
           {dados.name || "Perfil sem nome"}
